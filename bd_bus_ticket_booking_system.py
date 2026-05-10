c240a6af93c81480530b2d36c6fc7e1927b06540
arfinaleusuf <arfinaleusuf@gmail.com>
2026-05-11 01:41:39 +0600

    passenger and admin class

--- bd_bus_ticket_booking_system.py
+++ bd_bus_ticket_booking_system.py
@@ -15,3 +15,15 @@ class Bus:
                 return True
             else:
                 return False
+    
+class Passenger:
+    def __init__(self, name, phone, bus):
+        self.name = name
+        self.phone = phone
+        self.bus = bus
+
+class Admin:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
